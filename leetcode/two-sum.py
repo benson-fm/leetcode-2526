2737221e39104409b6d63b12fa71592871cfd553
Benson Manzano <bensonfmanzano@gmail.com>
2025-06-06 21:30:44 -0500

    Added two pointers

--- leetcode/two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        targets = dict()
-
-        for i in range(len(nums)):
-            if nums[i] in targets:
-                return [i, targets[nums[i]]]
-            else:
-                targets[target - nums[i]] = i
